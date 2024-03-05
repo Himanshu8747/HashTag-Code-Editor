55c795b3abd7886ed3572e2078a2028c9b43c244
Himanshu <himanshu8747@gmail.com>
2024-03-04 23:44:21 -0500

    Deploy

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,7 +6,11 @@ import { compilerRouter } from "./routes/compilerRouter";
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin:["https://deploy-mern-1whq.vercel.app"],
+  methods:["POST","GET"],
+  credentials:true
+}));
 config();
 
 app.use("/compiler", compilerRouter);
